cc24c6f61b38f34d0dde20eb83c67eba34af1038
specialyas <holla2specialyusuf@gmail.com>
2026-01-04 00:07:57 +0100

    add modification on user model

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import TIMESTAMP, Column, DateTime, Integer, String, ForeignKey, func, text
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, nullable=False, primary_key=True)
+    # username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    password = Column(String, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False,
+                        onupdate=func.now(),
+                        server_default=func.now(),
+    )
